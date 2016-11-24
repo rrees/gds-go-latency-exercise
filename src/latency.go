69796225741694a36141a64462a129b709d033f0
Robert Rees <shudderfix@gmail.com>
2016-11-24 16:06:27 +0000

    Actually sleeps on the latency endpoint

--- src/latency.go
+++ src/latency.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import "net/http"
+import "time"
 
 func main() {
   fmt.Println("Hello world")
@@ -12,5 +13,7 @@ func main() {
 }
 
 func defaultLatency(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Sleeping for 500ms")
+  fmt.Fprintf(w, "Sleeping for 500ms\n")
+  time.Sleep(time.Duration(500) * time.Millisecond)
+  fmt.Fprintf(w, "Sleep now complete!\n")
 }
